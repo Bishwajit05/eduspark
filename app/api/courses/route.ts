909a52b5937a160f63d0bcfb66141c0cd5e1a1fb
Bishwajit Behera <bbishwajit05@gmail.com>
2023-10-02 19:56:57 +0530

    Upload thing

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,11 +1,11 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { log } from "console";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
+    console.log("🚀 ~ file: route.ts:8 ~ POST ~ userId:", userId);
     const { title } = await req.json();
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
